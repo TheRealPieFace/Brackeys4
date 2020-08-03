ddcf929b02e473740f4a3a3aa68f5ba6d230a90c
TheRealPieFace <34294484+TheRealPieFace@users.noreply.github.com>
2020-08-03 10:32:37 -0400

    Useless Curve Paths

---
+++ Brackeys4/Assets/Editor/PathEditor.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PathCreator))]
+public class PathEditor : Editor
+{
+    PathCreator creator;
+    Path path;
+
+    private void OnSceneGUI()
+    {
+        Input();
+        Draw();
+    }
+
+    void Input()
+    {
+        Event guiEvent = Event.current;
+        Ray ray = HandleUtility.GUIPointToWorldRay(guiEvent.mousePosition);
+        RaycastHit hit = new RaycastHit();
+        
+        Vector3 mousePos = HandleUtility.GUIPointToWorldRay(guiEvent.mousePosition).origin;
+
+        if(guiEvent.type == EventType.MouseDown && guiEvent.button == 0 && guiEvent.shift)
+        {
+            
+            if (Physics.Raycast(ray, out hit, 1000))
+            {
+                Undo.RecordObject(creator, "Add segment");
+                path.AddSegment(new Vector3(hit.point.x, path.GetPointsInSegment(0)[0].y, hit.point.z));
+            }
+            
+            
+        }
+    }
+
+    void Draw()
+    {
+        for(int i = 0; i < path.NumSegments; i++)
+        {
+            Vector3[] points = path.GetPointsInSegment(i);
+            Handles.color = Color.white;
+            Handles.DrawLine(points[1], points[0]);
+            Handles.DrawLine(points[2], points[3]);
+            Handles.DrawBezier(points[0], points[3], points[1], points[2], Color.green, null, 2);
+        }
+
+        Handles.color = Color.red;
+        for(int i = 0; i < path.NumPoints; i++)
+        {
+            Vector3 newPos = Handles.FreeMoveHandle(path[i], Quaternion.identity, .1f, Vector3.zero, Handles.SphereHandleCap);
+            if(path[i] != newPos)
+            {
+                Undo.RecordObject(creator, "Move point");
+                path.MovePoint(i, new Vector3(newPos.x, path[i].y, newPos.z));
+            }
+        }
+    }
+
+    private void OnEnable()
+    {
+        creator = (PathCreator)target;
+        if(creator.path == null)
+        {
+            creator.CreatePath();
+        }
+        path = creator.path;
+    }
+}
---
+++ Brackeys4/Assets/Pierre/Scripts/BezierCurve/Path.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Path
+{
+    [SerializeField, HideInInspector]
+    List<Vector3> points;
+
+    public Path(Vector3 center)
+    {
+        points = new List<Vector3>
+        {
+            center + Vector3.left,
+            center + (Vector3.left + Vector3.forward) * .5f,
+            center + (Vector3.right + Vector3.back) * .5f,
+            center + Vector3.right
+        };
+    }
+
+    public Vector3 this[int i]
+    {
+        get
+        {
+            return points[i];
+        }
+    }
+
+    public int NumPoints 
+    { 
+        get 
+        {
+            return points.Count;
+        } 
+    }
+
+    public int NumSegments
+    {
+        get
+        {
+            return (points.Count - 4) / 3 + 1;
+        }
+    }
+
+    public void AddSegment(Vector3 anchorPos)
+    {
+        points.Add(points[points.Count - 1] * 2 - points[points.Count - 2]);
+        points.Add(points[points.Count - 1] + anchorPos * .5f);
+        points.Add(anchorPos);
+    }
+
+    public Vector3[] GetPointsInSegment(int i)
+    {
+        return new Vector3[] { points[i * 3], points[i * 3 + 1], points[i * 3 + 2], points[i * 3 + 3] };
+    }
+
+    public void MovePoint(int i, Vector3 pos)
+    {
+        Vector3 deltaMove = pos - points[i];
+        points[i] = pos;
+
+        if(i%3 == 0)
+        {
+            if(i+1 < points.Count)
+            {
+                points[i + 1] += deltaMove;
+            }
+            if(i-1 >= 0)
+            {
+                points[i - 1] += deltaMove;
+            }
+        }
+        else
+        {
+
+        }
+    }
+}
